66281d3ab3001d3e30ae777c46228f95f576ba28
Wish <breezewish@outlook.com>
2022-04-18 09:22:00 +0800

    Support specifying rows to be selected

--- payload/rand_point_get.go
+++ payload/rand_point_get.go
@@ -5,10 +5,13 @@ import (
 
 	"github.com/crazycs520/loadgen/cmd"
 	"github.com/crazycs520/loadgen/config"
+	"github.com/spf13/cobra"
 )
 
 type RandPointGetSuite struct {
 	*basicQuerySuite
+
+	selectRows int
 }
 
 func (c *RandPointGetSuite) Name() string {
@@ -19,8 +22,18 @@ func (c *RandPointGetSuite) GenQueryPrepareStmt() string {
 	return "select * from " + c.tblInfo.DBTableName() + " where a = ?;"
 }
 
+func (c *RandPointGetSuite) Cmd() *cobra.Command {
+	cmd := c.basicQuerySuite.Cmd()
+	cmd.Flags().IntVarP(&c.selectRows, "select-rows", "", -1, "the range of rows to be selected, -1 for all rows")
+	return cmd
+}
+
 func (c *RandPointGetSuite) GenQueryArgs() []interface{} {
-	return []interface{}{rand.Intn(c.rows)}
+	randRows := c.selectRows
+	if randRows < 0 {
+		randRows = c.rows
+	}
+	return []interface{}{rand.Intn(randRows)}
 }
 
 func NewRandPointGetSuite(cfg *config.Config) cmd.CMDGenerater {
