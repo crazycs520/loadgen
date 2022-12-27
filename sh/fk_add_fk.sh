dd70d0d51560698c791c74d769fc7b01af0732e6
crazycs520 <crazycs520@gmail.com>
2022-12-27 16:46:57 +0800

    add foreign key add fk test sh
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

---
+++ sh/fk_add_fk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOADGEN_BIN='../bin/loadgen'
+THREAD=20
+ROWS=10000000
+
+# prepare
+$LOADGEN_BIN payload fk-prepare
+$LOADGEN_BIN payload fk-insert-child --thread=$THREAD --rows=$ROWS
+
+echo '---------------- add fk speed --------------------'
+$LOADGEN_BIN exec --sql="alter table fk_child add foreign key (pid) references fk_parent(id)"
+
+echo '---------------- add fk without auto create index speed --------------------'
+$LOADGEN_BIN exec --sql="alter table fk_child add foreign key (pid) references fk_parent(id)"
+
+
+echo '---------------- add index speed  --------------------'
+$LOADGEN_BIN exec --sql="alter table fk_child add index idx0(pid)"
