5d93be37e4a4fed1ebe833b357837f536c6e002e
crazycs520 <crazycs520@gmail.com>
2022-12-27 16:53:23 +0800

    refine fk_insert_qps.sh
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- sh/fk_insert_qps.sh
+++ sh/fk_insert_qps.sh
@@ -5,8 +5,15 @@ THREAD=20
 ROWS=1000000
 
 $LOADGEN_BIN payload fk-prepare
-$LOADGEN_BIN payload fk-add-fk
+$LOADGEN_BIN exec --sql="alter table fk_child add foreign key fk_1 (pid) references fk_parent(id);"
+
+echo '------------- insert with fk check -----------------'
 $LOADGEN_BIN payload fk-insert-child --thread=$THREAD --rows=$ROWS --fk-check=true  --batch-size=1 
 sleep 5
+echo '------------- insert without fk check -----------------'
 $LOADGEN_BIN payload fk-insert-child --thread=$THREAD --rows=$ROWS --fk-check=false --batch-size=1 
+
+echo '------------- insert without fk constraint -----------------'
+$LOADGEN_BIN exec --sql="alter table fk_child drop foreign key fk_1;"
+$LOADGEN_BIN payload fk-insert-child --thread=$THREAD --rows=$ROWS --fk-check=true --batch-size=1 
 
