68e6e432f57a2185c4c0b150f5baec23ea6b5236
crazycs520 <crazycs520@gmail.com>
2022-12-30 14:33:12 +0800

    fk_delete_cascade_qps.sh
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

---
+++ sh/fk_delete_cascade_qps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOADGEN_BIN='../bin/loadgen'
+THREAD=20
+ROWS=1000000
+
+echo '---------------- delete with fk cascade --------------------'
+$LOADGEN_BIN payload fk-prepare --thread=$THREAD --rows=$ROWS
+$LOADGEN_BIN payload fk-insert-child --thread=$THREAD --rows=$ROWS --parent-row=$ROWS  --batch-size=100
+$LOADGEN_BIN exec --sql="alter table fk_child add foreign key (pid) references fk_parent(id) on delete cascade"
+$LOADGEN_BIN payload fk-delete-parent --thread=$THREAD --rows=$ROWS --fk-check=true
+
+echo '---------------- delete without fk check --------------------'
+
+$LOADGEN_BIN payload fk-prepare --thread=$THREAD --rows=$ROWS
+$LOADGEN_BIN payload fk-insert-child --thread=$THREAD --rows=$ROWS --parent-row=$ROWS  --batch-size=100
+$LOADGEN_BIN exec --sql="alter table fk_child add foreign key (pid) references fk_parent(id) on delete cascade"
+$LOADGEN_BIN payload fk-delete-parent --thread=$THREAD --rows=$ROWS --fk-check=false
+
+
+echo '---------------- delete without fk constraint --------------------'
+$LOADGEN_BIN payload fk-prepare --thread=$THREAD --rows=$ROWS
+$LOADGEN_BIN payload fk-delete-parent --thread=$THREAD --rows=$ROWS --fk-check=false
