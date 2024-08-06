d1b54d5dc1b1753cc4672e261799653bdf27c22f
crazycs520 <crazycs520@gmail.com>
2024-08-06 13:35:02 +0800

    refien
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- sysbench/bench_ob.sh
+++ sysbench/bench_ob.sh
@@ -8,6 +8,7 @@ TABLE_SIZE=1000000
 THREADS=(32 64 128)
 LOAD_TYPES=("oltp_point_select" "oltp_read_only" "oltp_read_write")
 
+# sysbench --config-file=sysbench.conf oltp_point_select --tables=32 --table-size=10000 --threads=128 prepare
 function do_sysbench() {
     echo "do sysbench $1, thread=$2"
     sysbench $1 run --config-file=$CONFIG_FILE --tables=$TABLES --table-size=$TABLE_SIZE --threads=$2 --report-interval=10 --mysql-ignore-errors=1062,2013,8028,9007
