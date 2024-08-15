363fd7e3fdae08bd7095f4e09c1d33b4b428afd4
crazycs520 <crazycs520@gmail.com>
2024-08-15 17:25:40 +0800

    add add index sh
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

---
+++ sysbench/add_global_partition_index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HOST=10.202.15.229
+PORT=2883
+USER=root@test
+DB=test
+
+function exec_sql() {
+    echo "exec sql: $1"
+    mysql -u $USER -h $HOST -P $PORT --password=$PASSWORD -e "$1"
+}
+
+for i in {1..32}
+do
+  for j in {1..8}
+  do
+    echo $i;
+    exec_sql "alter table $DB.sbtest$i add index k_$j_$i(k) global PARTITION BY RANGE(k)
+       (PARTITION p1 VALUES LESS THAN(1000000),
+         PARTITION p2 VALUES LESS THAN(2000000),
+         PARTITION p3 VALUES LESS THAN(3000000),
+         PARTITION p4 VALUES LESS THAN(4000000),
+         PARTITION p5 VALUES LESS THAN(5000000),
+         PARTITION p6 VALUES LESS THAN(6000000),
+         PARTITION p7 VALUES LESS THAN(7000000),
+         PARTITION p8 VALUES LESS THAN(8000000),
+         PARTITION p9 VALUES LESS THAN(9000000),
+         PARTITION p10 VALUES LESS THAN MAXVALUE
+       );"
+
+  done
+done
