64b266de703a64fb33d25f736304636096eb8da9
crazycs520 <crazycs520@gmail.com>
2024-08-15 23:57:31 +0800

    init
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- sysbench/oltp_common.lua
+++ sysbench/oltp_common.lua
@@ -199,7 +199,7 @@ CREATE TABLE sbtest%d(
   c CHAR(120) DEFAULT '' NOT NULL,
   pad CHAR(60) DEFAULT '' NOT NULL,
   %s (id)
-) partition by hash(id) partitions 32 %s]],
+) %s]],
       table_num, id_def, id_index_def, extra_table_options)
 
    con:query(query)
