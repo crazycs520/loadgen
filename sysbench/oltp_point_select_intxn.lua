0001c166dfcc9e1b900de2dd99710630254ae72d
crazycs520 <chenshuang@pingcap.cn>
2025-10-28 13:12:40 +0800

    add oltp_point_select_intxn.lua
    Signed-off-by: crazycs520 <chenshuang@pingcap.cn>

---
+++ sysbench/oltp_point_select_intxn.lua
@@ -0,0 +1,38 @@
+#!/usr/bin/env sysbench
+-- Copyright (C) 2006-2017 Alexey Kopytov <akopytov@gmail.com>
+
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+-- ----------------------------------------------------------------------
+-- Read-Only OLTP benchmark
+-- ----------------------------------------------------------------------
+
+require("oltp_common")
+
+function prepare_statements()
+   prepare_point_selects()
+   prepare_begin()
+   prepare_commit()
+end
+
+function event()
+   begin()
+
+   for i = 1, 10 do
+      execute_point_selects()
+   end
+
+   commit()
+end
